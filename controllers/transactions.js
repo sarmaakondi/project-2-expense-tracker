6ab6f1a5d9710a9d380d4d5ba2bcb3427f58f3f9
avnms <avnmsarma@gmail.com>
2024-06-25 20:25:31 +1000

    Created transactions home page

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,4 +3,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
+// Routes
+router.get("/", (req, res) => {
+  try {
+    res.render("transactions/index.ejs");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
