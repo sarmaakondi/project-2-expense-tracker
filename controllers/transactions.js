264b0bd0e24b372a7913d1cd542afd4d4f3ffabe
avnms <avnmsarma@gmail.com>
2024-06-25 21:12:44 +1000

    Implemented route to store new transaction data and tag the user created it

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Transaction = require("../models/transaction");
 
 // Routes
+// Route to transactions index page
 router.get("/", (req, res) => {
   try {
     res.render("transactions/index.ejs");
@@ -13,9 +15,25 @@ router.get("/", (req, res) => {
   }
 });
 
-// Display for to create new transaction
+// Route to display form to add new transaction
 router.get("/new", async (req, res) => {
   res.render("transactions/new.ejs");
+});
+
+// Route to handle and post the new transaction data
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const newTransactionData = {
+      ...req.body,
+      createdBy: currentUser._id,
+    };
+    const newTransaction = await Transaction.create(newTransactionData);
+    res.redirect(`/users/${currentUser._id}/transactions`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
